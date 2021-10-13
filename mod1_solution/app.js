5ca2e2cc4ce2a74bab9c50cafa5029dc1f2c1ed6
VICTOR <bankolevictor77@gmail.com>
2021-10-13 23:07:29 +0100

    module1_solution

---
+++ mod1_solution/app.js
@@ -0,0 +1,22 @@
+(function() {
+    "use strict";
+    angular
+        .module("LunchCheck", [])
+        .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ["$scope"];
+
+    function LunchCheckController($scope) {
+        $scope.lunchMessage = "";
+        $scope.lunchItems = "";
+
+        $scope.lunchCalculate = function() {
+            var message = $scope.lunchMessage;
+            var items = $scope.lunchItems;
+            if (items == 0) message = "please enter data first";
+            else if (items.split(",").length < 4) message = "Enjoy!";
+            else message = "Too much!";
+            $scope.lunchMessage = message;
+        };
+    }
+})();
